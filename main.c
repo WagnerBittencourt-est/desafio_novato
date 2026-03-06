afb879f0f9f29e1f36617b4e4d25442b1b3d0a2d
Carlos Wagner Gonçalves Bittencourt <263085854+WagnerBittencourt-est@users.noreply.github.com>
2026-03-06 19:47:05 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Carta {
+    char Estado[3];
+    char Codigo[4]; 
+    char Cidade[51];
+    int populacao;
+    double area;
+    double pib;
+    int numPontoTuristico;
+};
+  
+int sair() {
+     char opcao;
+
+    do {
+        printf("Digite 'S' para sair: ");
+        // O espaço antes do %c serve para ignorar o 'Enter' anterior no buffer
+        scanf(" %c", &opcao);
+        // O loop continua enquanto a opcao for DIFERENTE de 's' E 'S'
+    } while (opcao != 's' && opcao != 'S');
+
+    printf("\nVoce digitou '%c'. Saindo do programa...\n", opcao);
+    exit(0);
+}
+
+int main() {
+   system("clear");
+   struct Carta carta[2];
+   printf("Digite as informacoes para 2 cartas:\n\n");
+   for (int i = 0; i <= 1; i++) {
+        printf("\n");
+        printf("Carta: %d \n", (i + 1));
+
+        printf("Estado: \n");
+        scanf("%s", &carta[i].Estado);
+
+        printf("Codigo: \n");
+        scanf("%s", &carta[i].Codigo);
+
+        printf("Cidade: \n");
+        scanf("%s", &carta[i].Cidade);
+
+        printf("Populacao: \n");
+        scanf("%d", &carta[i].populacao);
+
+        printf("Area: \n");
+        scanf("%lf", &carta[i].area);
+
+        printf("PIB: \n");
+        scanf("%lf", &carta[i].pib);
+
+        printf("Numero de Pontos Turisticos: \n");
+        scanf("%d", &carta[i].numPontoTuristico);
+    }
+    
+    system("cls");
+
+    for (int i = 0; i <= 1; i++)
+    {
+        printf("---------------------------------\n\n");
+        printf("Carta %d:\n", i + 1);
+        printf("Estado: %s\n", carta[i].Estado);
+        printf("Codigo: %s\n", carta[i].Codigo);
+        printf("Cidade: %s\n", carta[i].Cidade);
+        printf("Populacao: %d\n", carta[i].populacao);
+        printf("Area: %.2lf\n", carta[i].area);
+        printf("PIB: %.2lf\n", carta[i].pib);
+        printf("Numero de Pontos Turisticos: %d\n\n\n", carta[i].numPontoTuristico);
+    }
+    printf("---------------------------------\n\n");
+   
+    sair();
+
+    return 0;
+}
